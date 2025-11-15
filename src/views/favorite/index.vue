6f295e8aa544c04e4bfee69793a9dfc2a151cbb8
zhoupeijun012 <zhoupeijun012@163.com>
2025-11-16 00:34:51 +0800

    2323

--- src/views/favorite/index.vue
+++ src/views/favorite/index.vue
@@ -2,7 +2,12 @@
   <div class="favorite">
     <ft-list :requestFunction="$api.getFocusList" ref="ft-list">
       <template v-slot:list="{ list }">
-        <stock-card v-for="item in list" :key="item.id" :info="item"></stock-card>
+        <stock-card
+          v-for="item in list"
+          :key="item.id"
+          :info="item"
+          @click.native="toDetail(item)"
+        ></stock-card>
       </template>
     </ft-list>
   </div>
@@ -12,22 +17,72 @@
 import FtList from "@/components/ft-list";
 import StockCard from "@/components/stock-card";
 export default {
-  name: 'favorite',
+  name: "favorite",
   components: {
     FtList,
     StockCard,
   },
   data() {
-    return {
-
-    };
+    return {};
   },
   mounted() {
     this.$refs["ft-list"].query({
       order: [{ prop: "f3", order: "descending" }],
       where: [],
-      matchKeys: ["f14","f3","f40014","f41006","f40006","f40008","f40009","f40010","f40011","f40012","f40013","f40017","f6","c_f103","f12","f21","f20","f2","f12","f7","f23","c_f100","c_f102","f8","f9","f11","f41003","f41004","f24","f62","f267","f164","f63","f17","f2","f15","f16","f5","f4","f41006","f40016"],
+      matchKeys: [
+        "f14",
+        "f3",
+        "f40014",
+        "f41006",
+        "f40006",
+        "f40008",
+        "f40009",
+        "f40010",
+        "f40011",
+        "f40012",
+        "f40013",
+        "f40017",
+        "f6",
+        "c_f103",
+        "f12",
+        "f21",
+        "f20",
+        "f2",
+        "f12",
+        "f7",
+        "f23",
+        "c_f100",
+        "c_f102",
+        "f8",
+        "f9",
+        "f11",
+        "f41003",
+        "f41004",
+        "f24",
+        "f62",
+        "f267",
+        "f164",
+        "f63",
+        "f17",
+        "f2",
+        "f15",
+        "f16",
+        "f5",
+        "f4",
+        "f41006",
+        "f40016",
+      ],
     });
+  },
+  methods: {
+    toDetail(item) {
+      this.$router.push({
+        name: "stock-detail",
+        query: {
+          f12: item.f12,
+        },
+      });
+    },
   },
 };
 </script>

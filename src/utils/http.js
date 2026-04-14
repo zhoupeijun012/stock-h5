ad09bd67c2e9a1a2d6029a1b325a6ec1a93d0a90
zhoupeijun012 <zhoupeijun012@163.com>
2026-04-14 21:43:45 +0800

    34

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,7 +6,7 @@ import { Toast } from "vant";
 const instance = axios.create({
   // baseURL: 'https://120.48.100.49/api',
 //   baseURL: '/api',
-  baseURL: 'http://120.48.100.49/api',
+  baseURL: 'https://stock-api.amdyes.asia/api',
   timeout: 60 * 1000,
 });
 
